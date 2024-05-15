6ed86348ddaa9bd830802725fc804586514ba730
bvgr003 <148019599+bvgr003@users.noreply.github.com>
2024-05-15 09:30:11 -0600

    Initial commit

---
+++ test.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    var x = document.querySelector(".navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+
+
+
+
+
+
+
+
